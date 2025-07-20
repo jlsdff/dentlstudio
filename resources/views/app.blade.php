4b95057072a174b7a0c095dcff92e168fe4559c1
julius <8215455@ntc.edu.ph>
2025-07-21 00:54:37 +0800

    preload fonts & define fonts

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -61,8 +61,19 @@
     <link rel="icon" href="/favicon.svg" type="image/svg+xml">
     <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+    <link rel="preload"
+          href={{ asset('/fonts/playfair-display.woff2') }}
+          as="font"
+          type="font/woff2"
+          crossorigin="anonymous"
+    />
+
+    <link rel="preload"
+          href={{ asset('/fonts/instrument-sans.woff2') }}
+          as="font"
+          type="font/woff2"
+          crossorigin="anonymous"
+    />
 
     @routes
     @viteReactRefresh
