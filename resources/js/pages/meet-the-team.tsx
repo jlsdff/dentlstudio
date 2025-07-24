68eea8ef1ef9e8c0ff95f423942ad1ce2a9ad428
julius <8215455@ntc.edu.ph>
2025-07-25 01:31:53 +0800

    redesign meet the team page

--- resources/js/pages/meet-the-team.tsx
+++ resources/js/pages/meet-the-team.tsx
@@ -62,6 +62,9 @@ export default function AboutUs() {
     const [scrolledPastHeader, setScrolledPastHeader] = useState(false)
     const header = useRef<HTMLHeadElement>(null)
 
+    const title = "Meet The Team Behind the The Dentl Studio"
+    const description = "Get to know the passionate dental professionals who make your smile their mission. Our skilled and caring team in Clyde North is dedicated to delivering exceptional care with a personal touch—every visit, every time."
+
     useEffect(() => {
         const handleScroll = () => {
             if (header.current) {
@@ -87,24 +90,67 @@ export default function AboutUs() {
                 <meta property="og:image" content="https://thedentlstudio.com/photos/thedentlstudio.jpg" />
                 <meta property="og:url" content="https://thedentlstudio.com/meet-the-team" />
                 <meta property="og:type" content="website" />
+
+                <link
+                    rel="preload"
+                    as="image"
+                    href="/social-staff-ad.webm"
+                    fetchPriority="high"
+                />
             </Head>
 
             <NavigationBar scrolled={scrolledPastHeader} />
 
-            <header
-                ref={header}
-                className="
-                bg-stone-950 text-soft-200 min-h-[50svh]
-                flex flex-col justify-end items-center
-                p-8 md:pt-18 gap-4 relative overflow-hidden
-                "
-            >
-                <h1 className="mt-12 md:mt-0  text-center text-4xl sm:text-5xl font-light font-serif tracking-widest" >
-                    Redefining Dental Care in Clyde North
-                </h1>
-                <p className="text-center">
-                    At The Dentl Studio, we combine innovation, precision, and compassion to deliver elevated dental experiences. Learn more about our story, values, and commitment to exceptional care.
-                </p>
+
+            <header ref={header} className="
+                 text-soft-200 h-[100svh]
+                grid grid-cols-1 md:grid-cols-8
+                ">
+
+                <div className="hidden md:flex md:col-span-5 p-8 sm:p-16 flex-col justify-end gap-4 relative
+                    bg-gradient-to-tr from-stone-950 to-stone-800">
+
+                    <h1 className="text-4xl md:text-5xl font-serif z-10">{title}</h1>
+                    <p className="z-10">
+                        {description}
+                    </p>
+                    <img src="/frosted.jpg" className="absolute w-full h-full object-cover top-0 left-0 mix-blend-multiply
+                        z-0" alt="frosted texture" aria-hidden />
+
+                </div>
+
+                <div className="md:col-span-3 overflow-y-hidden relative">
+                    <video
+                        className="absolute inset-0 w-full h-full object-cover"
+                        autoPlay
+                        loop
+                        muted
+                        playsInline
+                        poster="/thedentlstudio.jpg"
+                        preload="metadata"
+                        aria-hidden="true"
+                    >
+                        <source src="/social-staff-ad.webm" type="video/webm" />
+                    </video>
+                    <div className="absolute inset-0 w-full h-full flex md:hidden flex-col items-center justify-end p-8
+                        text-center gap-4 bg-gradient-to-t from-stone-950 to-transparent">
+                        <h1 className="text-4xl md:text-5xl font-serif z-10">{title}</h1>
+                        <p className="z-10">
+                            {description}
+                        </p>
+                        <div className="z-10">
+                            <a
+                                aria-label="Book an appointment at The Dentl Studio"
+                                className={`px-4 py-2 text-sm rounded-md bg-soft-800 text-soft-100 hover:bg-soft-900 transition-all duration-300 shadow-lg`}
+                                href='https://www.corepractice.is/practices/thedentlstudio/the-dentl-studio#/'
+                                target="_blank"
+                            >
+                                Book an Appointment
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
             </header>
 
             <main className="bg-soft-200 p-8 md:p-16">
